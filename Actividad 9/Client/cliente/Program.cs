0eec7b75af4e6f9c20fd22a04052386b08aaa1f9
Antonio Hernandez <62306021+Antonio180233@users.noreply.github.com>
2022-09-24 16:14:43 -0600

    Add files via upload

---
+++ Actividad 9/Client/cliente/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Conectar();
+			
+		}
+		
+		public static void Conectar() 
+		{
+			Socket miPrimerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			IPEndPoint miDireccion = new IPEndPoint(IPAddress.Parse("177.237.24.253"), 1234);
+			
+			byte[] textoAEnviar;
+			byte[] ByRec;
+			
+			string numero, fibo1 = "1", fibo2;
+			
+			try {
+	
+				miPrimerSocket.Connect(miDireccion);
+				Console.WriteLine("Succesfull conection");
+				
+				// capturar numero de fibonacci
+				Console.WriteLine("Ingresa la cantidad de numeros Fibonacci a imprimir: ");
+				numero = Console.ReadLine();
+				textoAEnviar = Encoding.Default.GetBytes(numero);
+				miPrimerSocket.Send(textoAEnviar, 0, textoAEnviar.Length, 0);
+				
+				int n = Int32.Parse(numero);
+				
+				Console.Clear();
+				
+				Console.WriteLine(fibo1);
+					//Enviar numero
+					do {
+				textoAEnviar = Encoding.Default.GetBytes(fibo1);
+				miPrimerSocket.Send(textoAEnviar, 0, textoAEnviar.Length, 0);
+				
+					//Recibe numero
+				ByRec = new byte[255];
+				int a  = miPrimerSocket.Receive(ByRec, 0, ByRec.Length, 0);
+				Array.Resize(ref ByRec, a);
+				fibo2 = Encoding.Default.GetString(ByRec);
+				
+				int x = Int32.Parse(fibo1);
+				int y = Int32.Parse(fibo2);
+				int z = x + y;
+				
+				fibo1 = z.ToString();
+				
+				Console.WriteLine(fibo1);
+				n--;
+					}while(n != 0);
+				miPrimerSocket.Close();
+				
+			} catch (Exception error) {
+				Console.WriteLine("Error: {0}", error.ToString());
+			}
+			
+			Console.WriteLine("press any key");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Actividad 9/Server/servidor/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servidor
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Conectar();
+		}
+		
+		public static void Conectar() 
+		{
+			byte[] ByRec;
+			Socket miPrimerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			IPEndPoint miDireccion = new IPEndPoint(IPAddress.Any, 1234);
+			
+			byte[] textoEnviar;
+			
+			string numero, fibo1, fibo2 = "1";
+			
+			try {
+				miPrimerSocket.Bind(miDireccion);
+				miPrimerSocket.Listen(1);
+				Console.WriteLine("Escuchando cliente...");
+				Socket Escuchar = miPrimerSocket.Accept();
+				Console.WriteLine("Conectado con exito.");
+				
+				// Recibe cantidad de series Fibonacci
+				ByRec = new byte[255];
+				int a  = Escuchar.Receive(ByRec, 0, ByRec.Length, 0);
+				Array.Resize(ref ByRec, a);
+				numero = Encoding.Default.GetString(ByRec);
+				
+				int n = Int32.Parse(numero);
+				
+				// Recibe numeros
+				do{
+				ByRec = new byte[255];
+				int b  = Escuchar.Receive(ByRec, 0, ByRec.Length, 0);
+				Array.Resize(ref ByRec, b);
+				fibo1 = Encoding.Default.GetString(ByRec);
+				
+				Console.WriteLine(fibo2);
+				
+				int x = Int32.Parse(fibo1);
+				int y = Int32.Parse(fibo2);
+				int z = y + x;
+				
+				fibo2 = z.ToString();
+				// Enviar numero
+				textoEnviar = Encoding.Default.GetBytes(fibo2);
+				Escuchar.Send(textoEnviar, 0, textoEnviar.Length, 0);
+				n--;
+				
+				}while(n != 0);
+
+				
+				miPrimerSocket.Close();
+				
+			} catch (Exception error) {
+				Console.WriteLine("Error: {0}", error.ToString());
+			}
+			
+			Console.WriteLine("Presiona cualquier tecla para cerrar");
+			Console.ReadLine();
+		}
+	}
+}
